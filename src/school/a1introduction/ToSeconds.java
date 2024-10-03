4776a79d2a92d5dd77e51ad7ece29377144deba3
Jana Vitvar <jana.vitvarova@tul.cz>
2024-10-03 09:43:52 +0200

    Add Scanner communication

--- src/school/a1introduction/ToSeconds.java
+++ src/school/a1introduction/ToSeconds.java
@@ -1,17 +1,28 @@
 
 package school.a1introduction;
+//Test: in: 1 1 1 out: 3661
+//Test: in: 25 45 21 out: 92721
+
+import java.util.Scanner;
 
 public class ToSeconds {
     public static void main(String[] args) {
         //Vstup
-        int pocetHodin = 10;
-        int pocetMinut = 10;
-        int pocetSekund = 10;
+        Scanner sc = new Scanner(System.in);
+//        int pocetHodin = 25;
+//        int pocetMinut = 45;
+//        int pocetSekund = 21;
+        System.out.println("Zadej pocet hodin minut a sekund:");
+        int pocetHodin = sc.nextInt();
+        int pocetMinut = sc.nextInt();
+        int pocetSekund = sc.nextInt();
+        final int SEC_IN_HOUR = 3600;
+        final int SEC_IN_MIN = 60;
         
         //Zpracovani 
-        int celkem = (pocetHodin * 3600) + (pocetMinut * 60) + pocetSekund;
+        int celkem = (pocetHodin * SEC_IN_HOUR) + (pocetMinut * SEC_IN_MIN) + pocetSekund;
         
         //Vystup
-        System.out.format("Čas závodníka je %d vteřin", celkem);
+        System.out.format("Čas závodníka je %d vteřin%n\n", celkem);
     }
 }
