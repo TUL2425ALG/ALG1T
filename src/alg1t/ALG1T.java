f8be11ea4a39ceea7d1d4265ffba76130e987d74
Stepan Zakouril <stepan.zakouril@tul.cz>
2024-10-03 09:01:55 +0200

    ukol I guess

---
+++ src/alg1t/ALG1T.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alg1t;
+
+/**
+ *
+ * @author steve
+ */
+public class ALG1T {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
