2862cee6267314903af709c5776c79e86d48f726
Jana Vitvar <jana.vitvarova@tul.cz>
2024-10-24 10:24:52 +0200

    while instead of for

--- src/school/a3branchingloops/U22PrimeNumber.java
+++ src/school/a3branchingloops/U22PrimeNumber.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package school.a3branchingloops;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Vitvarova-J-31c9
- */
 public class U22PrimeNumber {
 
     public static void main(String[] args) {
@@ -20,26 +12,32 @@ public class U22PrimeNumber {
 
         Scanner sc = new Scanner(System.in);
         boolean again = true;
-        
+
         while (again) {
+            //in
             System.out.println("Napiš KLADNÉ číslo, které chceš zjistit jestli je prvočíslo:");
             int number = sc.nextInt();
             boolean isPrimeNumber = true;
-
+            //processing
             if (number == 1) {
                 isPrimeNumber = false;
             }
-            for (int i = 2; i <= Math.sqrt(number) && isPrimeNumber; i++) {
-                if (number % i == 0) {
+            int possibleDivisor = 2;
+            while (possibleDivisor <= Math.sqrt(number) && isPrimeNumber){ //for (int i = 2; i <= Math.sqrt(number) && isPrimeNumber; i++) {
+                if (number % possibleDivisor == 0) {
                     isPrimeNumber = false;
                     //break;
                 }
+                possibleDivisor++;
             }
+            //out
             System.out.println("Číslo " + (isPrimeNumber ? "je" : "není") + " prvočíslo");
-            System.out.println("Chcete pokračovat? (A/N)");
             
+            //repeat app
+            System.out.println("Chcete pokračovat? (A, a)");
+//            String s = sc.next().toUpperCase();
+//            again = "A".equals(s);
             char input = sc.next().toUpperCase().charAt(0);
-            
             if (input != 'A') {
                 again = false;
             }
