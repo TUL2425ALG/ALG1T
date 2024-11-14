be0dd45a8399426d25660839f1fcd2c2542fde21
Jana Vitvar <jana.vitvarova@tul.cz>
2024-11-14 12:21:16 +0100

    load print array

--- src/school/a6array/ArrayTools.java
+++ src/school/a6array/ArrayTools.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package school.a6array;
 
-/**
- *
- * @author Vitvarova-J-31c9
- */
 public class ArrayTools {
 
     public static void main(String[] args) {
